a81cf73256ad52a26b98164ade9fdb2ca2e21c83
Wanpan <wanpan96@163.com>
2023-10-25 15:35:54 +0800

    refactor(vite): optimized code

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,7 @@
 import { makeAutoObservable } from "mobx";
-import { nanoid } from "nanoid";
 import routers from "@src/router/config";
-import { getRouterMap } from "@src/utils/index";
+import { getPathRecord } from "@src/utils/index";
+import { transformRouter } from "@src/utils/index";
 
 export class MobxStore {
   darkMode = false;
@@ -14,52 +14,12 @@ export class MobxStore {
 
   // 获取菜单、路由
   get getRouteMenu() {
-    const router: Route[] = [];
-    const menu: MenuItem[] = [];
-
-    function getFullRouter(arr: Route[], router: Route[], menu: MenuItem[], partePath = "") {
-      arr.forEach(element => {
-        const id = nanoid();
-        const { path, index, title, childrenList, notMenu, icon } = element;
-
-        let newPath = path as string;
-        // 多级嵌套 补全 /
-        if (path?.startsWith("/") && childrenList?.length) {
-          newPath = (path + "/").replace(/\/\/+/g, "/");
-        }
-
-        const routeObj = {
-          ...element,
-          id,
-          path: index ? partePath : newPath,
-          childrenList: [],
-        };
-        const menuObj = {
-          key: id,
-          path: index ? partePath : (path as string),
-          label: title ? title : "",
-          icon: icon,
-          children: [],
-        };
-
-        router.push(routeObj);
-        if (!notMenu) {
-          menu.push(menuObj);
-        }
-
-        if (childrenList?.length) {
-          getFullRouter(childrenList, routeObj.childrenList, menuObj.children, newPath);
-        }
-      });
-    }
-    getFullRouter(routers, router, menu);
-
-    return { router, menu: menu[0].children as MenuItem[] };
+    return transformRouter(routers);
   }
 
   // 获取面包屑
   get routerMap() {
-    return getRouterMap(this.getRouteMenu.router[0].childrenList as Route[]);
+    return getPathRecord(this.getRouteMenu.router[0].childrenList!);
   }
 
   setTheme(boolean: boolean) {
