a2d2f33cdb76c6ef3b5d1c72a0a9e99a15084c0f
Wanpan <wanpan96@163.com>
2024-02-19 17:09:53 +0800

    fix: rewrite is function

--- scripts/development.config.js
+++ scripts/development.config.js
@@ -7,9 +7,8 @@ export default {
     proxy: {
       "/proxy": {
         target: "",
-        rewrite: { "^/proxy": "" },
+        rewrite: path => path.replace(/^\/proxy/, ""),
         changeOrigin: true, // 控制服务器接收到的请求头中host字段的值
-        secure: false,
       },
     },
   },
