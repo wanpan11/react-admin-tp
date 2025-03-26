467919c266c80872ae0ba409d73cceebe2ea9df3
Wanpan <wanpan96@163.com>
2025-03-26 18:39:06 +0800

    🦄 refactor:  Optimize the Form component
    Signed-off-by: Wanpan <wanpan96@163.com>

--- src/components/FormFilter/index.tsx
+++ src/components/FormFilter/index.tsx
@@ -1,24 +1,20 @@
-import { memo, useEffect, useRef } from "react";
+import { memo, useRef } from "react";
 import { Button, FormInstance } from "antd";
 import classNames from "classnames";
 
 import FormList, { FormListProps } from "../FormList";
 
-export interface FormFilterProps extends Pick<FormListProps, "form" | "onOk" | "onForm" | "onValuesChange"> {
+export interface FormFilterProps extends Pick<FormListProps, "form" | "onOk" | "onForm" | "onValuesChange" | "initialValues" | "stageValues"> {
   className?: string;
   filterInfo: FormItem[];
   reset?: boolean;
   loading?: boolean;
   compact?: boolean;
   searchBtn?: boolean;
-  /**
-   * @description 用于阶段性设置表单值
-   */
-  stageValues?: FormListProps["initialValues"];
 }
 
 const FormFilter = (props: FormFilterProps) => {
-  const { className, filterInfo, reset, loading, compact = false, searchBtn = true, stageValues, onForm, ...resetProps } = props;
+  const { className, filterInfo, reset, loading, compact = false, searchBtn = true, onForm, ...resetProps } = props;
 
   const formInstance = useRef<FormInstance<any> | null>(null);
 
@@ -27,28 +23,18 @@ const FormFilter = (props: FormFilterProps) => {
     onForm?.(form);
   };
 
-  const getBtn = () => {
-    return (
-      <div className={compact ? "ml-10" : "absolute bottom-0 right-0"}>
-        {reset ? <Button htmlType="reset">重置</Button> : null}
+  const getBtn = () => (
+    <div className={compact ? "ml-10" : "absolute bottom-0 right-0"}>
+      {reset ? <Button htmlType="reset">重置</Button> : null}
 
-        <Button className="ml-6" htmlType="submit" type="primary" loading={loading}>
-          查询
-        </Button>
-      </div>
-    );
-  };
-
-  useEffect(() => {
-    if (stageValues && formInstance.current) {
-      formInstance.current.setFieldsValue(stageValues);
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [JSON.stringify(stageValues)]);
+      <Button className="ml-6" htmlType="submit" type="primary" loading={loading}>
+        查询
+      </Button>
+    </div>
+  );
 
   return (
-    <div className={classNames("box-border rounded-md bg-white p-6 dark:border dark:border-dark_border dark:bg-dark_bg", className)}>
+    <div className={classNames("box-border rounded-md bg-white p-6", className)}>
       <FormList
         colon={false}
         layout="inline"
--- src/components/FormList/index.tsx
+++ src/components/FormList/index.tsx
@@ -1,4 +1,4 @@
-import { memo, ReactElement, useEffect } from "react";
+import { memo, ReactElement, useEffect, useMemo } from "react";
 import { Button, Form, FormInstance, FormProps } from "antd";
 
 import { getFormElement } from "../tools";
@@ -14,7 +14,14 @@ export interface FormListProps extends Pick<FormProps, "form" | "colon" | "class
   submitNode?: ReactElement;
 
   itemInfo: FormItem[];
+  /**
+   * @description 只在初始化时生效
+   */
   initialValues?: FormProps["initialValues"];
+  /**
+   * @description 该值变化后会触发表单值更新
+   */
+  stageValues?: FormListProps["initialValues"];
   onOk?: FormProps["onFinish"];
   onForm?: (form: FormInstance) => void;
 }
@@ -29,6 +36,7 @@ const FormList = (props: FormListProps) => {
     itemInfo,
     labelCol,
     wrapperCol,
+    stageValues,
     initialValues,
 
     searchBtn,
@@ -41,9 +49,10 @@ const FormList = (props: FormListProps) => {
   } = props;
 
   const [insideForm] = Form.useForm();
+  const realForm = form ? form : insideForm;
 
+  // 只在初始化时生效
   useEffect(() => {
-    const realForm = form ? form : insideForm;
     onForm?.(realForm);
 
     if (initialValues) {
@@ -59,43 +68,62 @@ const FormList = (props: FormListProps) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  // 该值变化后会触发表单值更新
+  useEffect(() => {
+    if (stageValues && realForm) {
+      realForm.setFieldsValue(stageValues);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [JSON.stringify(stageValues)]);
+
+  /* ======================= render ========================= */
+  const submitBtnNode = useMemo(() => {
+    if (submitBtn) {
+      if (submitNode) {
+        return submitNode;
+      } else {
+        return (
+          <Button htmlType="submit" type="primary" block>
+            保存
+          </Button>
+        );
+      }
+    } else {
+      return null;
+    }
+  }, [submitBtn, submitNode]);
+
   return (
-    <Form
-      form={form ? form : insideForm}
-      labelCol={labelCol ? { span: labelCol } : undefined}
-      wrapperCol={wrapperCol ? { span: wrapperCol } : undefined}
-      onFinish={onOk}
-      {...restProps}
-    >
-      {itemInfo.map(e =>
-        e.hide ? null : e.type === "blockNode" ? (
-          <span key={typeof e.name === "string" ? e.name : e.name.join("_")}>{e.label}</span>
-        ) : (
+    <Form form={realForm} labelCol={labelCol ? { span: labelCol } : undefined} wrapperCol={wrapperCol ? { span: wrapperCol } : undefined} onFinish={onOk} {...restProps}>
+      {itemInfo.map((e, idx) => {
+        if (e.hide) {
+          return null;
+        }
+
+        if (e.type === "blockNode") {
+          return <span key={typeof e.name === "string" ? e.name || idx : e.name.join("_")}>{e.label}</span>;
+        }
+
+        return (
           <Form.Item
-            key={typeof e.name === "string" ? e.name : e.name.join("_")}
-            name={e.name ? e.name : undefined}
             label={e.label}
             extra={e.extra}
             rules={[e.rule]}
+            hidden={!(e.visible ?? true)}
+            name={e.name ? e.name : undefined}
             valuePropName={e.type === "switch" ? "checked" : "value"}
+            key={typeof e.name === "string" ? e.name || idx : e.name.join("_")}
             initialValue={e.type === "radio" ? e.options?.[0].value : e.type === "switch" ? true : undefined}
           >
             {getFormElement(e.type, e)}
           </Form.Item>
-        )
-      )}
+        );
+      })}
 
       {searchBtn}
 
-      {submitBtn ? (
-        submitNode ? (
-          submitNode
-        ) : (
-          <Button htmlType="submit" type="primary" block>
-            保存
-          </Button>
-        )
-      ) : null}
+      {submitBtnNode}
     </Form>
   );
 };
