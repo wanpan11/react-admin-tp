b946debbad83bbc42ba8eb9214d42e8c57df5b62
Wanpan <wanpan96@163.com>
2025-06-13 11:45:20 +0800

    feat: FormList update (#15)
    * feat: FormItem update
    
    * feat: FormList update
    
    * feat: update
    
    * feat: update

--- src/components/FormList/index.tsx
+++ src/components/FormList/index.tsx
@@ -3,12 +3,13 @@ import { Button, Form, FormInstance, FormProps } from "antd";
 
 import { getFormElement } from "../tools";
 
-export interface FormListProps extends Pick<FormProps, "form" | "colon" | "className" | "style" | "layout" | "labelAlign" | "onValuesChange" | "disabled"> {
+export interface FormListProps extends Omit<FormProps, "labelCol" | "wrapperCol"> {
   labelCol?: number;
   wrapperCol?: number;
 
   // 搜索
   searchBtn?: null | ReactElement;
+
   // 提交
   submitBtn?: boolean;
   submitNode?: ReactElement;
@@ -98,22 +99,24 @@ const FormList = (props: FormListProps) => {
   return (
     <Form form={realForm} labelCol={labelCol ? { span: labelCol } : undefined} wrapperCol={wrapperCol ? { span: wrapperCol } : undefined} onFinish={onOk} {...restProps}>
       {itemInfo.map((e, idx) => {
+        const key = e.name instanceof Array ? e.name.join("_") : e.name || idx;
+
         if (e.hide) {
           return null;
         }
 
         if (e.type === "blockNode") {
-          return <span key={typeof e.name === "string" ? e.name || idx : e.name.join("_")}>{e.label}</span>;
+          return <span key={key}>{e.label}</span>;
         }
 
         return (
           <Form.Item
+            key={key}
+            name={e.name}
             label={e.label}
             extra={e.extra}
             rules={[e.rule]}
             hidden={!(e.visible ?? true)}
-            name={e.name ? e.name : undefined}
-            key={typeof e.name === "string" ? e.name || idx : e.name.join("_")}
             valuePropName={e.valuePropName ? e.valuePropName : e.type === "switch" ? "checked" : "value"}
             initialValue={e.type === "radio" ? e.options?.[0].value : e.type === "switch" ? true : undefined}
             {...e.formItemProps}
