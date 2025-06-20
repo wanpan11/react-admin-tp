84bbc50e8eca580c02ddb2fd87cf3ad6c8068633
wanpan11 <wanpan96@163.com>
2025-06-20 05:54:13 +0000

    Deploying to gh-pages from @ wanpan11/react-admin-tp@a81db3a49fbf25b39a194bfff3ce8a1a2aabf5fb 🚀

---
+++ js/423_4819a0df4877f0db78ce.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[423],{7423:(e,t,s)=>{s.r(t),s.d(t,{default:()=>l});var c=s(4128),i=s(1746),n=s(6106);const l=function({title:e}){return(0,n.jsx)("div",{children:(0,n.jsx)(c.A,{title:e,children:(0,n.jsx)(i.A,{active:!0})})})}}}]);
