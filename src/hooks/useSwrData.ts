ba5d8cd3ded33dae589e6ce1862ff73e6d518a5a
Wanpan <wanpan96@163.com>
2025-07-03 18:55:12 +0800

    feat: useSwrData 优化

--- src/hooks/useSwrData.ts
+++ src/hooks/useSwrData.ts
@@ -1,5 +1,6 @@
 import type { Key, KeyedMutator, SWRConfiguration } from "swr";
 import { DEFAULT_PAGE } from "&src/config";
+
 import { useCallback, useMemo, useState } from "react";
 import useSwr from "swr";
 
@@ -10,16 +11,19 @@ interface PageType {
   pageSize: number;
 }
 
-export interface UseSwrDataProps<P = any, R = any> {
+export interface UseSwrDataFullProps<P = any, R = any> {
   reqKey: SimpleKey; // 请求的 key 可以是字符串或数组
   req: (params: P) => Promise<AxiosRes<R>>; // 请求函数 返回 Promise<AxiosRes<R>>
   ready?: boolean; // 是否准备就绪，默认为 true
-  paging?: boolean; // 是否分页，默认为 false
   params?: SearchType<P>; // 受控请求参数 受控
+  // paging 相关配置
+  paging?: boolean; // 是否分页，默认为 false
   defaultPage?: PageType; // 默认分页信息 非受控
   defaultSearch?: SearchType<P>; // 默认搜索信息 非受控
-  swrConfig?: SWRConfiguration; // swr 配置项
+  // swr 配置项
+  swrConfig?: SWRConfiguration;
 }
+export type UseSwrDataProps<P = any, R = any> = Omit<UseSwrDataFullProps<P, R>, "paging" | "defaultPage" | "defaultSearch">;
 
 interface UseSwrData<R = any> {
   data?: R;
@@ -35,9 +39,10 @@ interface UseSwrPagIngDataPage<P = any, R = any> extends UseSwrData<R> {
   setSearch: React.Dispatch<React.SetStateAction<SearchType<P> | undefined>>;
 }
 
-export function useSwrData<R = any, P = any>(props: UseSwrDataProps<P, R> & { paging: true }): UseSwrPagIngDataPage<P, R>;
+export function useSwrData<R = any, P = any>(props: UseSwrDataFullProps<P, R> & { paging: true }): UseSwrPagIngDataPage<P, R>;
 export function useSwrData<R = any, P = any>(props: UseSwrDataProps<P, R> & { paging?: false }): UseSwrData<R>;
-export function useSwrData<R = any, P = any>(props: UseSwrDataProps<P, R>): UseSwrData<R> | UseSwrPagIngDataPage<P, R> {
+
+export function useSwrData<R = any, P = any>(props: UseSwrDataFullProps<P, R>): UseSwrData<R> | UseSwrPagIngDataPage<P, R> {
   const { reqKey, req, params, ready = true, paging = false, defaultSearch, defaultPage = DEFAULT_PAGE, swrConfig } = props;
 
   const [pageInfo, setPage] = useState(defaultPage);
@@ -48,42 +53,21 @@ export function useSwrData<R = any, P = any>(props: UseSwrDataProps<P, R>): UseS
       return null;
     }
 
-    if (Array.isArray(reqKey)) {
-      if (paging) {
-        return [reqKey, params, pageInfo, searchInfo];
-      }
-      else {
-        return [reqKey, params];
-      }
+    let mergeParams: any = {};
+    if (paging) {
+      mergeParams = { ...pageInfo, ...searchInfo, ...params };
     }
     else {
-      if (paging) {
-        return { key: reqKey, ...params, ...pageInfo, ...searchInfo };
-      }
-      else {
-        return { key: reqKey, ...params, ...pageInfo, ...searchInfo };
-      }
+      mergeParams = params;
     }
+
+    return [reqKey, mergeParams];
   }, [pageInfo, paging, params, ready, reqKey, searchInfo]);
 
   const { data, isLoading, error, mutate } = useSwr(
     mergeKey,
-    async (data: (P & { key?: SimpleKey }) | [SimpleKey, P]) => {
-      if (Array.isArray(data)) {
-        let sendData = {};
-        const paramsList = data.slice(1) as [P];
-        paramsList.forEach((item) => {
-          if (typeof item === "object" && item) {
-            sendData = { ...sendData, ...item };
-          }
-        });
-
-        return req(sendData as P);
-      }
-      else {
-        delete data.key;
-        return req(data);
-      }
+    async (data: [SimpleKey, P]) => {
+      return req(data[1]);
     },
     swrConfig || { revalidateOnFocus: false }
   );
