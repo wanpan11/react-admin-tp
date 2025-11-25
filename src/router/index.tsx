f2b9be174d648bf4936ef47bb0327b2f0a8a3ce3
Wanpan <wanpan96@163.com>
2025-11-25 18:42:32 +0800

    feat: add layer update eslint FormItem (#24)
    * feat: update FormItem
    
    * feat: add layer update eslint
    
    * feat: update dark-mode style

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,23 +1,25 @@
 import Error from "&src/components/Error";
-
 import ErrorBoundary from "&src/components/ErrorBoundary";
 import GLOBAL_ROUTERS, { getRoute } from "&src/router/config";
 import { useGetRouterConfig } from "&src/store";
+import { StyleProvider } from "@ant-design/cssinjs";
 import { BrowserRouter, Routes } from "react-router-dom";
 
 function AppRouter() {
   const { router } = useGetRouterConfig();
 
   return (
-    <ErrorBoundary errComponent={<Error />}>
-      <BrowserRouter basename={process.env.PUBLIC_ROUTER_BASE_NAME || undefined} future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
-        <Routes>
-          {getRoute(GLOBAL_ROUTERS.LOGIN_PAGE)}
-          {getRoute(GLOBAL_ROUTERS.NOT_FOUND_PAGE)}
-          {getRoute(router)}
-        </Routes>
-      </BrowserRouter>
-    </ErrorBoundary>
+    <StyleProvider layer>
+      <ErrorBoundary errComponent={<Error />}>
+        <BrowserRouter basename={process.env.PUBLIC_ROUTER_BASE_NAME || undefined} future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
+          <Routes>
+            {getRoute(GLOBAL_ROUTERS.LOGIN_PAGE)}
+            {getRoute(GLOBAL_ROUTERS.NOT_FOUND_PAGE)}
+            {getRoute(router)}
+          </Routes>
+        </BrowserRouter>
+      </ErrorBoundary>
+    </StyleProvider>
   );
 }
 
