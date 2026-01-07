9a17f8f1f4489cb7d4fde2a60ad1014372dc07ee
Wanpan <wanpan96@163.com>
2026-01-07 10:36:35 +0800

    chore: Optimize code and update dependencies (#25)
    * feat: 优化 createUiSlice
    
    * feat: update deps
    
    * feat: 优化样式
    
    * feat: 优化样式
    
    * feat: update
    
    * feat: update

--- src/layout/sider.tsx
+++ src/layout/sider.tsx
@@ -5,17 +5,8 @@ import { useEffect, useMemo, useState } from "react";
 const { Sider } = Layout;
 
 function SiderCom({ menu, selectKey }: { menu: MenuItem[]; selectKey: string | null }) {
-  const [keys, setKeys] = useState<string[]>([]);
-
-  const currentKay = useMemo(() => {
-    if (!selectKey) {
-      return menu[0].children ? menu[0].children[0]?.key : menu[0].key;
-    }
-
-    return selectKey;
-  }, [menu, selectKey]);
-
-  useEffect(() => {
+  // 默认展开所有有子菜单的菜单项
+  const defaultOpenKeys = useMemo(() => {
     const arr: string[] = [];
 
     menu.forEach((e) => {
@@ -24,21 +15,35 @@ function SiderCom({ menu, selectKey }: { menu: MenuItem[]; selectKey: string | n
       }
     });
 
-    // eslint-disable-next-line react-hooks/set-state-in-effect
-    setKeys(arr);
+    return arr;
   }, [menu]);
+
+  const [openKeys, setOpenKeys] = useState<string[]>(defaultOpenKeys);
+
+  // 每当默认展开的菜单项变化时，更新状态
+  useEffect(() => {
+    setOpenKeys(defaultOpenKeys);
+  }, [defaultOpenKeys]);
+
+  // 计算当前选中的菜单项
+  const currentSelectKey = useMemo(() => {
+    // 如果没有传入选中的 key，则默认选中第一个菜单项
+    if (!selectKey) {
+      return menu[0].children ? menu[0].children[0]?.key : menu[0].key;
+    }
+
+    return selectKey;
+  }, [menu, selectKey]);
 
   return (
     <Sider theme="light" width={160}>
       <Menu
         mode="inline"
         className="mt-5"
-        openKeys={keys}
-        selectedKeys={[`${currentKay}`]}
+        openKeys={openKeys}
+        selectedKeys={[`${currentSelectKey}`]}
         items={menu as MenuProps["items"]}
-        onOpenChange={(openKeys) => {
-          setKeys(openKeys);
-        }}
+        onOpenChange={setOpenKeys}
       />
     </Sider>
   );
